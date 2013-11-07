09766b089991cc62ec4881f53677202a711b89d0
Ben Burwell <bburwell1@gmail.com>
2013-11-07 11:16:22 -0500

    Delete all associated printers when an organization is deleted

--- actions/organizationDelete.js
+++ actions/organizationDelete.js
@@ -1,6 +1,6 @@
 exports.action = {
     name: 'organizationDelete',
-    description: 'Deletes an Organization',
+    description: 'Deletes an Organization and all associated Printers',
     version: 1,
     inputs: {
         required: [ 'organizationId' ],
@@ -8,9 +8,26 @@ exports.action = {
     },
     outputExample: {},
     run: function (api, connection, next) {
+
         var id = api.mongoose.Types.ObjectId(connection.params.organizationId);
-        api.mongoose.model('Organization').remove({ _id: id }, function (err) {
-            next(connection, true);
-        });
+        
+        api.mongoose.model('Organization')
+            .findOne({ _id: id })
+            .populate({ path: 'printers', model: 'Printer' })
+            .exec(function (err, org) {
+                if (org) {
+
+                    if (org.printers) {
+                        for (var i = 0; i < org.printers.length; i++) {
+                            org.printers[i].remove();
+                        }
+                    }
+
+                    org.remove();
+
+                }
+                next(connection, true);
+            }
+        );
     }
 };
